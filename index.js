8fc3564d4939e16c1b7399d96d3bb5ccc1e8262b
danjhkim <danjhkim@gmail.com>
2021-08-07 03:28:18 -0400

    commit 2

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStragtey = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
+
+// appying strategy to passport as middleware
+//using keys.js importing the id and key
+//callbackurl is the route that the user will sent to after they recieve auth from google. So what page on ur site.
+passport.use(
+	new GoogleStragtey(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.GoogleClientSecret,
+			callbackURL: '/auth/google/callback',
+		},
+		accessToken => {
+			console.log(accessToken);
+		},
+	),
+);
 
 app.get('/', (req, res) => {
 	res.send({ hi: 'there!' });
