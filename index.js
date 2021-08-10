eeac49708914a01872e1ca3139a31f3378a90a76
danjhkim <danjhkim@gmail.com>
2021-08-09 23:20:24 -0400

    updated 2

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStragtey = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
+
+//condense require just loads everything and runs it
+require('./services/passport');
+require('./models/User');
+
+const authRoutes = require('./routes/authRoutes');
+const { Passport } = require('passport');
 
 const app = express();
 
-// appying strategy to passport as middleware
-//using keys.js importing the id and key
-//callbackurl is the route that the user will sent to after they recieve auth from google. So what page on ur site.
-passport.use(
-	new GoogleStragtey(
-		{
-			clientID: keys.googleClientID,
-			clientSecret: keys.GoogleClientSecret,
-			callbackURL: '/auth/google/callback',
-		},
-		accessToken => {
-			console.log(accessToken);
-		},
-	),
+//middleware cookie setting encrypt and how long it says 30 days
+//extracting cookiedata
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: keys.cookieKey,
+	}),
 );
+//both required to apply the initalization and persistent login session.
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'there!' });
-});
+mongoose
+	.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(result => console.log('connected to db'))
+	.catch(err => console.log(err));
+
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-	console.log('Listening port 5000');
+	console.log(`Listening port ${PORT}`);
 });
